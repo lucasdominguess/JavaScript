7576a7bb0b70021edfba657aff314f45ef62acf9
lucasdominguess <lucasdominguesofficial@gmail.com>
2023-08-17 07:01:25 -0300

    17/08

---
+++ Curso Javascript (Luiz Otavio)/For.js
@@ -0,0 +1,10 @@
+// tipo mais simples de for 
+for (let i = 0 ; i<= 5 ; i++) { 
+    console.log(i)
+}
+// Como exibir lista com o seu respectivo indice 
+let time = ['corinthias ', 'palmeiras' , 'flamengo' , 'santos ', 'sao paulo']
+
+for (let i = 0 ; i < time.length ; i++) {
+    console.log(i,time[i])
+}
--- Curso Javascript (Luiz Otavio)/exercicio/index.js
+++ Curso Javascript (Luiz Otavio)/exercicio/index.js
@@ -1,26 +1,26 @@
 //resolvendo exercicio formulario 
 
-function meuEscopo () { 
+function cadastro () { 
     const form = document.querySelector('.form');
     const resultado =document.querySelector('.resultado');
        
     
     function recebeEventoForm(evento) { 
         evento.preventDefault(); 
-      
+                                //pegando class dos elementos no html
             const nome = form.querySelector('.nome');
             const sobrenome = form.querySelector('.sobrenome');
             const idade = form.querySelector('.idade');
-            const pessoas = [];
-            pessoas.push({
+            const pessoas = []; //arrays vazia para armazenar cadastro
+            pessoas.push({ //adcionando dados no array
                 nome: nome.value ,
-                sobrenome: sobrenome.value,
+                sobrenome: sobrenome.value, //pegando valor
                 idade: idade.value, 
             })
     
     
-            console.log(pessoas);
-    
+           // console.log(pessoas);
+            
             resultado.innerHTML+= `<p> Nome : ${nome.value} <br> Sobrenome: ${sobrenome.value} <br> Idade:  ${idade.value} </p>` ;   
           
         }
@@ -28,25 +28,4 @@ function meuEscopo () {
        form.addEventListener('submit',recebeEventoForm);
 }
     
-meuEscopo()   
-
-
-
-
-// function cadastrarFuncionario (nome,sobrenome,idade) {
-//     return  {
-//         nome ,  sobrenome ,idade , 
-       
-        
-//      }
-
-// }
-
-
-// let nome1 = document.getElementById('nome')
-// let sobrenome1 = document.getElementById('sobrenome')
-// let idade1 = document.getElementById('idade')
-
-// cadastro1 = cadastrarFuncionario (nome1,sobrenome1,idade1)
-
-// console.log(cadastro1)
+cadastro()   
